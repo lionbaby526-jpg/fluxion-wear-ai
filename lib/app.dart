9b701b71147e4fa84be440c2bf156d6d1558499a
lionbaby526-jpg <lionbaby526@gmail.com>
2026-07-06 16:48:19 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
+class FluxionWearAI extends StatelessWidget {
+  const FluxionWearAI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fluxion Wear AI',
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
