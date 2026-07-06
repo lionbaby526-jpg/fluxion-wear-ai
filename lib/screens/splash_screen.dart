384ab5adf55d181e9717d82b2da258149fc370e1
lionbaby526-jpg <lionbaby526@gmail.com>
2026-07-06 16:50:43 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const HomeScreen(),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
         child: Text(
-          'Fluxion Wear AI',
+          "Fluxion Wear AI",
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.bold,
