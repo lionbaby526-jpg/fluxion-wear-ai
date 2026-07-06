0c8dcfce6dd7a0a54092d5ca0ce6a6aae0b45254
lionbaby526-jpg <lionbaby526@gmail.com>
2026-07-06 16:53:50 +0530

    Update ble_service.dart

--- lib/services/ble_service.dart
+++ lib/services/ble_service.dart
@@ -1,1 +1,18 @@
 
+class BleService {
+  bool isConnected = false;
+
+  Future<void> scanDevices() async {
+    print("Scanning for BLE devices...");
+  }
+
+  Future<void> connect(String deviceId) async {
+    print("Connecting to $deviceId...");
+    isConnected = true;
+  }
+
+  Future<void> disconnect() async {
+    print("Disconnected");
+    isConnected = false;
+  }
+}
