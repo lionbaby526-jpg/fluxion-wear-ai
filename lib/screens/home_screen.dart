850b97b5f7c766caf3c58827ffae1d6102729645
lionbaby526-jpg <lionbaby526@gmail.com>
2026-07-06 16:49:55 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fluxion Wear AI"),
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to Fluxion Wear AI",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
